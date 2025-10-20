57090d25d0c686f676be8de50239f749e19907e1
iurizero <iuri.j.t@outlook.com>
2025-10-20 19:09:22 -0300

    Solve some exercises

---
+++ Exercícios 2/2.py
@@ -0,0 +1,5 @@
+def mostrar_nome():
+    nome = str(input("Digite o seu nome: "))
+    print(f"Olá {nome}! seja bem vindo")
+    
+mostrar_nome()
---
+++ freetest.py
@@ -0,0 +1,6 @@
+lista =[1, 2, 5, 6, 77, 2, 13, 55]
+
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
